f5e2b7172851bb6afef40805026667b91841ea6c
eugenyleschenko <57004179+eugenyleschenko@users.noreply.github.com>
2020-05-16 15:28:33 +0300

    Add files via upload

--- Осевые линии.cs
+++ Осевые линии.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/* 
+Eugeny Leschenko
+NIP-Informatica, 2020
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,15 +21,13 @@ using System.Collections;
 namespace Tekla.Technology.Akit.UserScript
 {
     public class Script
-    {
-        static readonly TSD.DrawingHandler drawingHandler = new TSD.DrawingHandler();
-        static readonly TSM.Model model = new TSM.Model();
-		
+    {	
 		public static void Run(Tekla.Technology.Akit.IScript akit)
         {
-			 var currentDrawing = drawingHandler.GetActiveDrawing();
+			TSD.DrawingHandler drawingHandler = new TSD.DrawingHandler();
+            var currentDrawing = drawingHandler.GetActiveDrawing();
 
-            DrawingObjectEnumerator parts = GetSelectedParts();
+            DrawingObjectEnumerator parts = Extensions.GetSelectedParts();
 
             foreach (var obj in parts)
             {
@@ -33,185 +36,102 @@ namespace Tekla.Technology.Akit.UserScript
                 if (part != null)
                 {
                     ViewBase view = part.GetView();
-                    CenterLinesDrawer cld = new CenterLinesDrawer(part, view);
-                    if (GetPartProfileType(part).Contains("R"))
+                    CenterLinesDrawer drawer = new CenterLinesDrawer(part, view);
+                    // если деталь - труба или пруток
+                    if (Extensions.GetPartProfileType(part).Contains("R")) 
                     {
-                        cld.SetDrawerMethod(new RoundPartCenterLinesDrawer());
+                        drawer.SelectDrawerMethod(new RoundBeamCenterLinesDrawer());
+                    }                    
+                    else
+                    // если деталь - пластина
+                    if (Extensions.SelectModelObject(part.ModelIdentifier.ID) is TSM.ContourPlate || 
+                        Extensions.GetPartProfileType(part).Contains("B"))
+                    {
+                        drawer.SelectDrawerMethod(new PlateCenterLinesDrawer());
                     }
                     else
-                    if (!GetPartProfileType(part).Contains("B"))
                     {
-                        cld.SetDrawerMethod(new ProfilePartCenterLinesDrawer());                       
+                        drawer.SelectDrawerMethod(new BeamCenterLinesDrawer());
                     }
-                    else
-                    if (SelectModelObject(part.ModelIdentifier.ID) is TSM.ContourPlate || GetPartProfileType(part).Contains("B"))
-                    {
-                        cld.SetDrawerMethod(new PlateCenterLinesDrawer());
-                    }
-
-                    cld.DrawPartCenterLines();
+                    drawer.DrawPartCenterLines();
                 }
             }
+            currentDrawing.CommitChanges();        
+        }
+    }
 
-            currentDrawing.CommitChanges();	          
-        }
-		 internal static DrawingObjectEnumerator GetSelectedParts()
-        {
-            Drawing activeDrawing = drawingHandler.GetActiveDrawing();
-            ViewBase sheet = activeDrawing.GetSheet();
-            DrawingObjectEnumerator objects = sheet.GetAllObjects();
-            objects.Reset();
-            DrawingObjectSelector dos = drawingHandler.GetDrawingObjectSelector();
-            objects = dos.GetSelected();
-            return objects;
-        }
-        
-        internal static string GetPartProfileType(TSD.Part drawingPart)
-        {
-            TSM.Part modelPart = SelectModelObject(drawingPart.ModelIdentifier.ID)
-                as TSM.Part;
-            string profileType = String.Empty;
-            modelPart.GetReportProperty("PROFILE_TYPE", ref profileType);
-            return profileType;
-        }        
-        internal static TSM.ModelObject SelectModelObject(int objectId)
-        {
-            TSM.ModelObject result = null;
-            TSM.ModelObject modelObject =
-                new TSM.Model().SelectModelObject(new Identifier(objectId));
-            result = modelObject;
-            return result;
-        }
-     }
-	  class CenterLinesDrawer
-    {        
-        TSD.Part _drawingPart;
-        ViewBase _viewBase;              
-        private IDrawCenterLines _drawCenterLines;
-        double _partWidth;
-        double _partHeight;
-        double _partLength;        
-        double[] _partStart;
-        double[] _partEnd;
-        CoordinateSystem _partCS;
+    class CenterLinesDrawer
+    {             
+        private IDrawerMethod _drawCenterLines;        
+        TsPart _tsPart;
+        TsView _tsView;
         
         public CenterLinesDrawer(TSD.Part drawingPart, ViewBase viewBase)
-        {
-            _drawingPart = drawingPart;
-            _viewBase = viewBase;            
+        {            
+            _tsPart = new TsPart(drawingPart);
+            _tsView = new TsView(viewBase);
         }        
-        public void SetDrawerMethod(IDrawCenterLines drawCenterLines)
+        public void SelectDrawerMethod(IDrawerMethod drawCenterLines)
         {
             this._drawCenterLines = drawCenterLines;
         }
         public void DrawPartCenterLines()
-        {
-            ProcessPart();
-
-            this._drawCenterLines.DrawCenterLines(_viewBase, _partStart, _partEnd,
-                _partWidth, _partHeight, _partLength, _partCS);
-
-        }
-                
-        /// <summary>
-        /// Возможно, использование этого метода - не лучшее решение и нужно было написать
-        /// отдельный класс для процессинга парта
-        /// </summary>
-        internal void ProcessPart()
-        {
-            TSM.Part modelPart = 
-                Extensions.GetModelObjectFromDrawingObject(_drawingPart) as TSM.Part;
-
-            _partCS = modelPart.GetCoordinateSystem();
-
-            modelPart.GetReportProperty("WIDTH", ref _partWidth);
-            modelPart.GetReportProperty("HEIGHT", ref _partHeight);
-            modelPart.GetReportProperty("LENGTH", ref _partLength);
-
-            var partCLPoints = modelPart.GetCenterLine(false);
-            Point partStart = partCLPoints[0] as Point;
-            Point partEnd;
-
-            // Двойная проверка: и в main, и здесь
-            if (!(modelPart is TSM.ContourPlate))
-            {
-                partEnd = partCLPoints[partCLPoints.Count - 1] as Point;
-            }
-            else
-            {
-                // Это сработает только с 4-хугольными пластинами, расположеными перпеникулярно плоскости вида
-                partEnd = partCLPoints[partCLPoints.Count - 2] as Point;
-            }
-            _partStart = new double[3] { partStart.X, partStart.Y, partStart.Z };
-            _partEnd = new double[3] { partEnd.X, partEnd.Y, partEnd.Z };
-
+        {    
+            this._drawCenterLines.DrawCenterLines(_tsPart, _tsView);
         }
     }    
 
-    interface IDrawCenterLines
+    interface IDrawerMethod
     {
-        /// <summary>
-        /// Основная проблема в том, что некоторым классам нужны не все аргументы
-        /// Возможно, стратегия - не лучший паттерн для решения задачи и надо реализовать фабрику
-        /// </summary>
-        /// <param name="view"></param>
-        /// <param name="start"></param>
-        /// <param name="end"></param>
-        /// <param name="width"></param>
-        /// <param name="height"></param>
-        /// <param name="length"></param>
-        /// <param name="partCS"></param>
-        void DrawCenterLines(ViewBase view, 
-            double[] start, double[] end,
-            double width, double height, 
-            double length,
-            CoordinateSystem partCS);
+        void DrawCenterLines(TsPart tsPart, TsView tsView);
     }
-
-    class PlateCenterLinesDrawer : IDrawCenterLines
+    
+    class PlateCenterLinesDrawer : IDrawerMethod
     {
-        public void DrawCenterLines(ViewBase view, double[] start, double[] end, 
-            double width, double height, double length, CoordinateSystem partCS)
+        public void DrawCenterLines(TsPart tsPart, TsView tsView)
         {
-            // Дублирование кода
-            double scale = ((View)view).Attributes.Scale;
+            
+            double scale = tsView.scale;
             double delta = 2 * scale;
 
-            // Бессмысленное действие: представление точки как массива 
-            // в классе CenterLinesDrawer и обратно, как точки здесь
+            double[] start = tsPart.partStart;
+            double[] end = tsPart.partEnd;
+
+            Point o = tsPart.origin;
+            
             Point startPoint = new Point(start[0], start[1], start[2]);
             Point endPoint = new Point(end[0], end[1], end[2]);
-
-            // Дублирование кода
-            Point o =
-                new Point((end[0] + start[0]) / 2, (end[1] + start[1]) / 2, (end[2] + start[2]) / 2);
-            // Вспомогательный LineSegment
+            
             LineSegment auxLS = new LineSegment(o, endPoint);
-            // Вспомогательный вектор направления
+            
             Vector auxVec = auxLS.GetDirectionVector();
-            // Векторы для растягивания конечных точек за пределы солида
-            Vector clVec = Extensions.ExtendedVector(auxVec, delta);
+            
+            Vector clVec = Extensions.ExtendVector(auxVec, delta);
             Vector nclVec = (-1) * clVec;
-            // Растягивание конечных точек за пределы солида
+            
             startPoint.Translate(nclVec.X, nclVec.Y, nclVec.Z);
             endPoint.Translate(clVec.X, clVec.Y, clVec.Z);
 
-            Drawer.DrawLine(view, startPoint, endPoint);  
+            Inserter.InsertLine(tsView._viewBase, startPoint, endPoint);  
         }
     }
 
-    class ProfilePartCenterLinesDrawer : IDrawCenterLines
+    class BeamCenterLinesDrawer : IDrawerMethod
     {
-        public void DrawCenterLines(ViewBase view, double[] start, double[] end,
-            double width, double height, double length, CoordinateSystem partCS)
+        public void DrawCenterLines(TsPart tsPart, TsView tsView)
         {
-            // Дублирование кода
-            double scale = ((View)view).Attributes.Scale;
+            
+            double scale = tsView.scale;
             double delta = 2 * scale;
+            
+            Point o =tsPart.origin;
 
-            //Point o = partCS.Origin;
-            Point o =
-                new Point((end[0] + start[0]) / 2, (end[1] + start[1]) / 2, (end[2] + start[2]) / 2);
+            CoordinateSystem partCS = tsPart.partCS;
+
+            double width = tsPart.width;
+            double height = tsPart.height;
+            double length = tsPart.length;
+
             var x = new Point(o);
             var y = new Point(o);
             var z = new Point(o);
@@ -225,9 +145,9 @@ namespace Tekla.Technology.Akit.UserScript
             Vector rZ = 
                 Extensions.RoundVector(Vector.Cross(partCS.AxisX, partCS.AxisY).GetNormal()) * width * 0.5;
 
-            rX = Extensions.ExtendedVector(rX, delta);
-            rY = Extensions.ExtendedVector(rY, delta);
-            rZ = Extensions.ExtendedVector(rZ, delta);
+            rX = Extensions.ExtendVector(rX, delta);
+            rY = Extensions.ExtendVector(rY, delta);
+            rZ = Extensions.ExtendVector(rZ, delta);
 
             Vector nrX = (-1) * rX;
             Vector nrY = (-1) * rY;
@@ -241,44 +161,47 @@ namespace Tekla.Technology.Akit.UserScript
             ny.Translate(nrY.X, nrY.Y, nrY.Z);
             nz.Translate(nrZ.X, nrZ.Y, nrZ.Z);
 
-            Drawer.DrawLine(view, nx, x);
-            Drawer.DrawLine(view, ny, y);
-            Drawer.DrawLine(view, nz, z);
+            ViewBase view = tsView._viewBase;
+
+            Inserter.InsertLine(view, nx, x);
+            Inserter.InsertLine(view, ny, y);
+            Inserter.InsertLine(view, nz, z);
         }
     }
 
-    class RoundPartCenterLinesDrawer : IDrawCenterLines
+    class RoundBeamCenterLinesDrawer : IDrawerMethod
     {
-        public void DrawCenterLines(ViewBase view, double[] start, double[] end,
-            double width, double height, double length, CoordinateSystem partCS)
+        public void DrawCenterLines(TsPart tsPart, TsView tsView)
         {
-            // Дублирование кода
-            double scale = ((View)view).Attributes.Scale;
+            
+            double scale = tsView.scale;
+            double width = tsPart.width;
+            double height = tsPart.height;
+            ViewBase view = tsView._viewBase;
+            
             double delta = Math.Max(width, height)/2+2*scale;
-
-            Point partCenter = 
-                new Point((end[0]+start[0])/2,(end[1]+start[1])/2, (end[2]+start[2])/2);
+            Point partCenter = tsPart.origin;
 
             Point iStart = new Point(partCenter.X + delta, partCenter.Y, partCenter.Z);
             Point iEnd = new Point(partCenter.X - delta, partCenter.Y, partCenter.Z);
             
-            Drawer.DrawLine(view, iStart, iEnd);
+            Inserter.InsertLine(view, iStart, iEnd);
              
             Point jStart = new Point(partCenter.X, partCenter.Y + delta, partCenter.Z);
             Point jEnd = new Point(partCenter.X, partCenter.Y- delta, partCenter.Z);
 
-            Drawer.DrawLine(view, jStart, jEnd); 
+            Inserter.InsertLine(view, jStart, jEnd); 
             
             Point kStart = new Point(partCenter.X, partCenter.Y, partCenter.Z + delta);
             Point kEnd = new Point(partCenter.X, partCenter.Y, partCenter.Z - delta);
 
-            Drawer.DrawLine(view, kStart, kEnd);
+            Inserter.InsertLine(view, kStart, kEnd);
         }
     }
 
-    class Drawer
+    class Inserter
     {
-        internal static void DrawLine(ViewBase view, Point start, Point end)
+        internal static void InsertLine(ViewBase view, Point start, Point end)
         {
             if (start != null && end != null)
             {
@@ -296,16 +219,98 @@ namespace Tekla.Technology.Akit.UserScript
             }
         }     
     }
+    public class TsPart
+    {
+        public TSD.Part _drawingPart;
+        public readonly double width;
+        public readonly double height;
+        public readonly double length;
+        public readonly double[] partStart;
+        public readonly double[] partEnd;
+        public readonly CoordinateSystem partCS;
+        public Point origin;
 
+        public TsPart(TSD.Part drawingPart)
+        {
+            _drawingPart = drawingPart;
+            TSM.Part modelPart =
+               Extensions.GetModelObjectFromDrawingObject(_drawingPart) as TSM.Part;
+
+            modelPart.GetReportProperty("WIDTH", ref width);
+            modelPart.GetReportProperty("HEIGHT", ref height);
+            modelPart.GetReportProperty("LENGTH", ref length);
+
+            var partCLPoints = modelPart.GetCenterLine(false);
+            Point partStartP = partCLPoints[0] as Point;
+            Point partEndP;
+            
+            if (!(modelPart is TSM.ContourPlate))
+            {
+                partEndP = partCLPoints[partCLPoints.Count - 1] as Point;
+            }
+            else
+            {                
+                partEndP = partCLPoints[partCLPoints.Count - 2] as Point;
+            }
+            partStart = new double[3] { partStartP.X, partStartP.Y, partStartP.Z };
+            partEnd = new double[3] { partEndP.X, partEndP.Y, partEndP.Z };
+
+            partCS = modelPart.GetCoordinateSystem();
+            origin =
+                new Point((partEnd[0] + partStart[0]) / 2, 
+                (partEnd[1] + partStart[1]) / 2, 
+                (partEnd[2] + partStart[2]) / 2);
+        }
+    }
+
+    public class TsView
+    {
+        public ViewBase _viewBase;
+        public double scale;
+
+        public TsView(ViewBase viewBase)
+        {
+            _viewBase = viewBase;
+            scale = ((View)_viewBase).Attributes.Scale;
+        }
+    }
     class Extensions
     {
+        internal static DrawingObjectEnumerator GetSelectedParts()
+        {
+            TSD.DrawingHandler drawingHandler = new TSD.DrawingHandler();
+            Drawing activeDrawing = drawingHandler.GetActiveDrawing();
+            ViewBase sheet = activeDrawing.GetSheet();
+            DrawingObjectEnumerator objects = sheet.GetAllObjects();
+            objects.Reset();
+            DrawingObjectSelector dos = drawingHandler.GetDrawingObjectSelector();
+            objects = dos.GetSelected();
+            return objects;
+        }
+
+        internal static string GetPartProfileType(TSD.Part drawingPart)
+        {
+            TSM.Part modelPart = SelectModelObject(drawingPart.ModelIdentifier.ID)
+                as TSM.Part;
+            string profileType = String.Empty;
+            modelPart.GetReportProperty("PROFILE_TYPE", ref profileType);
+            return profileType;
+        }
+        internal static TSM.ModelObject SelectModelObject(int objectId)
+        {
+            TSM.ModelObject result = null;
+            TSM.ModelObject modelObject =
+                new TSM.Model().SelectModelObject(new Identifier(objectId));
+            result = modelObject;
+            return result;
+        }
         internal static TSM.ModelObject GetModelObjectFromDrawingObject(TSD.ModelObject drawingObject)
         {
-            TSM.ModelObject modelObject = 
-                (new TSM.Model()).SelectModelObject(drawingObject.ModelIdentifier);            
+            TSM.ModelObject modelObject =
+                (new TSM.Model()).SelectModelObject(drawingObject.ModelIdentifier);
             return modelObject;
         }
-        
+
         internal static Point TransformPointToDisplay(Point point, ViewBase view)
         {
             if (point != null)
@@ -317,7 +322,7 @@ namespace Tekla.Technology.Akit.UserScript
                 return result;
             }
             return null;
-        } 
+        }
         internal static Vector RoundVector(Point vector)
         {
             var result = new Vector
@@ -328,12 +333,12 @@ namespace Tekla.Technology.Akit.UserScript
             };
             return result;
         }
-        internal static Vector ExtendedVector(Vector v, double ext)
+        internal static Vector ExtendVector(Vector v, double ext)
         {
             double vLen = Math.Sqrt(Math.Pow(v.X, 2) + Math.Pow(v.Y, 2) + Math.Pow(v.Z, 2));
             Vector u = new Vector(v.X / vLen, v.Y / vLen, v.Z / vLen);
 
             return new Vector(u.X * (vLen + ext), u.Y * (vLen + ext), u.Z * (vLen + ext));
-        }        
-    }
+        }
+    } 	 
  }
